f05324f3e67a83cb6921f65aca9224af0f49d512
Nathan Gilbert <weneedmoreminerals@gmail.com>
2021-06-06 18:43:23 -0400

    Fixed 3

--- backend/db/test_db.py
+++ backend/db/test_db.py
@@ -2,6 +2,6 @@ from .db_utils import exec_get_one
 
 def show_single(username):
     return exec_get_one('''
-        SELECT * FROM public."Test_Test"
+        SELECT username FROM public."Test_Test"
             WHERE username = %s;
     ''', (username,))
