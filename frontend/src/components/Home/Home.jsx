325b042679b6b46e7ce6894a0f8e77873c937cbb
Sean Schukraft <sschukra@oswego.edu>
2021-07-07 01:21:11 -0400

    Created 'Home' branch with very basic home layout

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import logo from './img/logo.svg';
-
+import { Container, Row, Col } from 'reactstrap';
+import './Home.css';
 
 
 class Home extends Component {
 
   render() {
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Link className="App-link" to="/login">
-          Login
-        </Link>
-        </header>
+      <div className="bg">
+        <Container fluid>
+          <Row>
+            <Col xs="2" style={{backgroundColor: '#494949', color: 'white', paddingBottom: "100%"}}>Talks</Col>
+            <Col xs = "8" style={{backgroundColor: '#555555', color: 'white'}}>Settings</Col>
+            <Col xs="2" style={{backgroundColor: '#494949', color: 'white'}}>Friends</Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
